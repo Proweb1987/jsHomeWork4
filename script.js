ec8c10feb411da206cd1cbb2386560150db9218d
vi <87@.ru>
2022-01-05 21:53:21 +0500

    finish

--- script.js
+++ script.js
@@ -1,9 +1,48 @@
 
-function vi() {
-    let name = prompt ('введите ваше имя');
-    let now = +prompt ('ведите текущий год');
-    let age = +prompt ('введите свой год рождения');
-    let answer = name + (',') + 'ваш возраст ' + (now - age);
-    return answer;
+// function vi() {
+//     let name = prompt ('введите ваше имя');
+//     let now = +prompt ('ведите текущий год');
+//     let age = +prompt ('введите свой год рождения');
+//     let answer = name + (',') + 'ваш возраст ' + (now - age);
+//     return answer;
+// }
+// console.log(vi());
+
+
+
+
+function random(min = 1,max = 10) {
+   return Math.floor(Math.random() * (max - min) + min);
+   
 }
-console.log(vi());
+
+
+let amount = +prompt ('введите количество примеров');
+
+   
+
+for(let i = 0;i < amount;i++) {
+   let number1 = random();
+   let number2 = random();
+   
+     
+  if(i == 0) {        
+  let minus = +prompt (number1 + ' - ' + number2 + ' = ');
+  let answer = (number1 - number2) == minus ? ' ваш ответ верный ' : ' ваш ответ не верный ' + minus + '!' + ' правильный ответ ';
+  console.log(answer + (number1 - number2));
+  }else if(i == 1) {
+     let delenie = +prompt (number1 + ' / ' + number2 + ' = ');
+     let answer = (number1 / number2) == delenie ? ' ваш ответ верный ' : ' ваш ответ не верный ' + delenie + '!' + ' правильный ответ ';
+  console.log(answer + (number1 / number2));
+  }else if(i == 2) {
+    let umnoj = +prompt(number1 + ' * ' + number2 + ' = ');
+    let answer = (number1 * number2) == umnoj ? ' ваш ответ верный ' : ' ваш ответ не верный ' + umnoj + '!' + ' правильный ответ ';
+    console.log(answer + (number1 * number2));
+  }else if(i == 0 ){
+   let primer = +prompt(number1 + ' + ' + number2 + ' = ');
+   let answer = (number1 + number2) == primer ? ' ваш ответ верный ' : ' ваш ответ не верный ' + primer + '!' + ' правильный ответ ';
+   console.log(answer + (number1 + number2));
+  }
+}
+   
+
